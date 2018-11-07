7c1d45da0d39dfd57b9461dde3b709143fa4d21f
avidkenil <avidkenil@gmail.com>
2018-11-06 20:11:08 -0500

    made changes to save the obs in enjoy.py

--- enjoy.py
+++ enjoy.py
@@ -1,11 +1,26 @@
 import argparse
 import os
+import sys
 
 import numpy as np
 import torch
+import pickle
+
 
 from envs import VecPyTorch, make_vec_envs
 from utils import get_render_func, get_vec_normalize
+
+
+def save_object(object, filepath):
+	'''
+	This is a defensive way to write pickle.write, allowing for very large files on all platforms
+	'''
+	max_bytes = 2**31 - 1
+	bytes_out = pickle.dumps(object, protocol=4)
+	n_bytes = sys.getsizeof(bytes_out)
+	with open(filepath, 'wb') as f_out:
+		for idx in range(0, n_bytes, max_bytes):
+			f_out.write(bytes_out[idx:idx+max_bytes])
 
 
 parser = argparse.ArgumentParser(description='RL')
@@ -57,22 +72,43 @@ if args.env_name.find('Bullet') > -1:
         if (p.getBodyInfo(i)[0].decode() == "torso"):
             torsoId = i
 
-while True:
-    with torch.no_grad():
-        value, action, _, recurrent_hidden_states = actor_critic.act(
-            obs, recurrent_hidden_states, masks, deterministic=args.det)
+final_video = np.array([])
+desired_frames = 10000
+count = 0
+try:
+    while True:
+        with torch.no_grad():
+            value, action, _, recurrent_hidden_states = actor_critic.act(
+                obs, recurrent_hidden_states, masks, deterministic=args.det)
 
-    # Obser reward and next obs
-    obs, reward, done, _ = env.step(action)
+        # Obser reward and next obs
+        obs, reward, done, _ = env.step(action)
+        #print(type(obs.numpy()))
+        save_obs = np.swapaxes(obs.numpy(),0,1)[np.newaxis,:].astype(np.int16)
+        print(save_obs.shape)
+        print(final_video.shape)
+        final_video = np.vstack((final_video,save_obs)) if final_video.size is not 0 else save_obs
+        print(final_video.shape)
+        print(final_video.dtype)
 
-    masks.fill_(0.0 if done else 1.0)
+        #np.save(open('obs.npy','wb'),obs)
 
-    if args.env_name.find('Bullet') > -1:
-        if torsoId > -1:
-            distance = 5
-            yaw = 0
-            humanPos, humanOrn = p.getBasePositionAndOrientation(torsoId)
-            p.resetDebugVisualizerCamera(distance, yaw, -20, humanPos)
+        masks.fill_(0.0 if done else 1.0)
 
-    if render_func is not None:
-        render_func('human')
+        if args.env_name.find('Bullet') > -1:
+            if torsoId > -1:
+                distance = 5
+                yaw = 0
+                humanPos, humanOrn = p.getBasePositionAndOrientation(torsoId)
+                p.resetDebugVisualizerCamera(distance, yaw, -20, humanPos)
+
+        if render_func is not None:
+            render_func('human')
+        count+=1
+        if(count >= desired_frames):
+            save_object(final_video,'saved_videos/PongVideo.pkl')
+            break
+
+except KeyboardInterrupt:
+    print('Keyboard Interupped. File will now be saved')
+    save_object(final_video,'saved_videos/PongVideo.pkl')
